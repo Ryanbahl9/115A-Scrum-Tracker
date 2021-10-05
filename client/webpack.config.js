263864c5485dc7eaff2efc245ebf2775463fc567
Niland Schumacher <lando@Nilands-MacBook-Air.local>
2021-10-05 10:24:32 -0700

    removed script tag from src html, causing an error saying bundle.js was not found

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,42 +3,34 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
+  entry: './src/index.js',
   mode: "development",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve("dist"),
-    publicPath: "/",
-  },
   devServer: {
     static: "./dist",
-
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: "babel-loader",
       },
       {
-        test: /\.html$/,
+        test: /\.html$/i,
         use: "html-loader",
       },
     ],
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './dist/index.html'
+      template: '/src/index.html'
     })
   ],
   resolve: {
     extensions: [".js"],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    path: __dirname + '/dist',
   },
 };
