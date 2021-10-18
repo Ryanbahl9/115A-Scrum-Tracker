0906f50e1b20af043f443da25212b4f1fb6f3735
Brandon Kwe <bkwe@DESKTOP-JEO1C3A.localdomain>
2021-10-18 16:43:54 -0700

    basic outline of add user story, non-funcitonal

--- src/components/Backlog.js
+++ src/components/Backlog.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import AddBoxSharpIcon from '@mui/icons-material/AddBoxSharp';
+import Stack from '@mui/material/Stack';
+import {createUserStory} from './UserStory';
 
 const Backlog = () => {
     return (
-        <>
-            <h1>
-                The backlog bitch!
-            </h1>
-        </>
+        <div>
+            <section>
+                <h1/> {/* White space styling */}
+                <Stack direction="row" container justifyContent="flex-end">
+                    <Button variant="contained" endIcon={<AddBoxSharpIcon />} onClick = {createUserStory} >
+                        Add User Story
+                    </Button>
+                </Stack>
+            </section>
+        </div>
     )
 }
     
