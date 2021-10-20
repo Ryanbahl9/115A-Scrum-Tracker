2519bca9a22196c4d6d52db8f4f6bd22865ed6ae
Brandon Kwe <bkwe@DESKTOP-JEO1C3A.localdomain>
2021-10-20 14:52:32 -0700

    dynamic input page

--- src/components/UserStory.js
+++ src/components/UserStory.js
@@ -6,99 +6,103 @@ import TextField from '@mui/material/TextField';
 import {useState} from 'react';
 
 const UserStoryInput = (props) => {
-    const inputOpen = props;
-
+    const {inputOpen} = props;
+    console.log(inputOpen);
     const [value, setValue] = React.useState('Controlled');
   
     const handleChange = (event) => {
       setValue(event.target.value);
     };
-    return (
-    <Box
-        component="form"
-        sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-    >
-        <div>
-        <TextField
-            id="outlined-multiline-flexible"
-            label="Multiline"
-            multiline
-            maxRows={4}
-            value={value}
-            onChange={handleChange}
-        />
-        <TextField
-            id="outlined-textarea"
-            label="Multiline Placeholder"
-            placeholder="Placeholder"
-            multiline
-        />
-        <TextField
-            id="outlined-multiline-static"
-            label="Multiline"
-            multiline
-            rows={4}
-            defaultValue="Default Value"
-        />
-        </div>
-        <div>
-        <TextField
-            id="filled-multiline-flexible"
-            label="Multiline"
-            multiline
-            maxRows={4}
-            value={value}
-            onChange={handleChange}
-            variant="filled"
-        />
-        <TextField
-            id="filled-textarea"
-            label="Multiline Placeholder"
-            placeholder="Placeholder"
-            multiline
-            variant="filled"
-        />
-        <TextField
-            id="filled-multiline-static"
-            label="Multiline"
-            multiline
-            rows={4}
-            defaultValue="Default Value"
-            variant="filled"
-        />
-        </div>
-        <div>
-        <TextField
-            id="standard-multiline-flexible"
-            label="Multiline"
-            multiline
-            maxRows={4}
-            value={value}
-            onChange={handleChange}
-            variant="standard"
-        />
-        <TextField
-            id="standard-textarea"
-            label="Multiline Placeholder"
-            placeholder="Placeholder"
-            multiline
-            variant="standard"
-        />
-        <TextField
-            id="standard-multiline-static"
-            label="Multiline"
-            multiline
-            rows={4}
-            defaultValue="Default Value"
-            variant="standard"
-        />
-        </div>
-    </Box>
-    );
+    if (inputOpen) {
+        return (
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <div>
+                    <TextField
+                    id="outlined-multiline-flexible"
+                    label="Multiline"
+                    multiline
+                    maxRows={4}
+                    value={value}
+                    onChange={handleChange}
+                    />
+                    <TextField
+                    id="outlined-textarea"
+                    label="Multiline Placeholder"
+                    placeholder="Placeholder"
+                    multiline
+                    />
+                    <TextField
+                    id="outlined-multiline-static"
+                    label="Multiline"
+                    multiline
+                    rows={4}
+                    defaultValue="Default Value"
+                    />
+                </div>
+                <div>
+                    <TextField
+                    id="filled-multiline-flexible"
+                    label="Multiline"
+                    multiline
+                    maxRows={4}
+                    value={value}
+                    onChange={handleChange}
+                    variant="filled"
+                    />
+                    <TextField
+                    id="filled-textarea"
+                    label="Multiline Placeholder"
+                    placeholder="Placeholder"
+                    multiline
+                    variant="filled"
+                    />
+                    <TextField
+                    id="filled-multiline-static"
+                    label="Multiline"
+                    multiline
+                    rows={4}
+                    defaultValue="Default Value"
+                    variant="filled"
+                    />
+                </div>
+                <div>
+                    <TextField
+                    id="standard-multiline-flexible"
+                    label="Multiline"
+                    multiline
+                    maxRows={4}
+                    value={value}
+                    onChange={handleChange}
+                    variant="standard"
+                    />
+                    <TextField
+                    id="standard-textarea"
+                    label="Multiline Placeholder"
+                    placeholder="Placeholder"
+                    multiline
+                    variant="standard"
+                    />
+                    <TextField
+                    id="standard-multiline-static"
+                    label="Multiline"
+                    multiline
+                    rows={4}
+                    defaultValue="Default Value"
+                    variant="standard"
+                    />
+                </div>
+            </Box>
+        )
+    } else {
+        return <Box/>
+    }
 }
 
   export default UserStoryInput
