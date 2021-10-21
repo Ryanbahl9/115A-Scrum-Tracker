65c90a26f062dd399a855b06871239360a9a4ee8
Niland Schumacher <lando@littlemouse.local>
2021-10-21 12:18:28 -0700

    added some basic styling, nothing fancy

--- src/components/ScrumBoard.js
+++ src/components/ScrumBoard.js
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useRef, useReducer } from 'react'
 import { firestore } from './fire';
 import UserContext from './UserContext';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import UserStoryRow from "./UserStoryRow"
+import { doc, updateDoc } from "firebase/firestore";
+import { Container, Button, Box, TextField } from '@mui/material';
 
 
 const Board = () => {
+    const newColumnRef = useRef();
+    const [numStages, setNumStages] = useState();
     let { product } = useContext(UserContext);
+
     const userStoryRef = firestore.collection('userStory');
     let query;
     if(product){
@@ -16,37 +21,46 @@ const Board = () => {
     }
     let [UserStories, loading, error] = useCollectionData(query);
 
-    const addColumn = () => {
-        //functionality to add a 'stage' goes here
+
+    const addColumn = async (e) => {
+        if (newColumnRef.current.value.length === 0) return;
+        e.preventDefault();
+        const productRef = doc(firestore, "products", product.id);
+        await updateDoc(productRef, {
+            stages: [...product.stages, newColumnRef.current.value]
+        });
     }
 
     const renderStageTitles = () => {
-        if (!product) return
-        let StageTitles = [<div key={0}>User Stories</div>, <div key={1}>To Do</div>];
+        let StageTitles = [<Box sx={{ paddingTop: "30px", textAlign: "center", width: "200px", borderBottom: "1px solid black", borderRight: "1px solid black" }} key={0}>User Stories</Box>, <Box sx={{ paddingTop: "30px", textAlign: "center",width: "200px", borderBottom: "1px solid black", borderRight: "1px solid black" }} key={1}>To Do</Box>];
         if(product.stages){
-            StageTitles = StageTitles.concat(product.stages.map((stageTitle) => <div>{stageTitle}</div>));
+
+            StageTitles = StageTitles.concat(product.stages.map((stageTitle, i) => <Box sx={{ paddingTop: "30px", textAlign: "center",width: "200px", borderBottom: "1px solid black", borderRight: "1px solid black"  }} key={i+2}>{stageTitle}</Box>));
         }
-        StageTitles.push(<div key={StageTitles}>Completed</div>);
+        StageTitles.push(<Box sx={{ paddingTop: "30px", textAlign: "center",width: "200px", borderBottom: "1px solid black" }} key={StageTitles.length}>Completed</Box>);
         return StageTitles;
     }
 
     return (<>{
         product ?
-            (<div>
+            (<Container sx={{background:"aliceBlue", height:"91vh", width:"100vw"}}>
                 <div>
                     sprint selector
                 </div>
-                <div>
+                <Box sx={{width:"100%", display:'flex', justifyContent:"center", marginTop:"20px"}}>
                     {renderStageTitles()}
-                    <div onClick={addColumn}>
-                        + add column
-                    </div>
-                </div>
-                <div>
+                    <Box sx={{ paddingLeft:"50px"}}>
+                        <TextField id="outlined-basic" label="Add Stage" variant="outlined" />
+                        <Button variant="outlined" onClick={addColumn}>
+                            + add column
+                        </Button>
+                    </Box>
+                </Box>
+                <Box sx={{ width: "100%"}}>
                     {loading && <div>user stories are loading</div> }
                     {!loading && UserStories?.map((story, i) => { return (<UserStoryRow key={i} doc={story}></UserStoryRow>) })}
-                </div>
-            </div >)
+                </Box>
+            </Container >)
         :
             <div>You must select a project to view the board</div>
         }</>)
--- src/components/UserStoryRow.js
+++ src/components/UserStoryRow.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Box } from '@mui/material';
 
 const UserStoryRow = (props) => {
     //this component will query the tasks collection to get all the tasks associated with this user story
     //will be adding soon
     return (
-        <div>
-            {props.doc.testing}
-        </div>
+        <Box sx={{ scrollSnapAlign: "start",
+                   background:"white",
+                   marginTop: "10px",
+                   marginBottom: "10px",
+                   width: "100%",
+                   height: "200px",
+                   borderRadius: "15px",
+                   boxShadow:"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"}}>
+        empty user story row
+        </Box>
     )
 }
 
