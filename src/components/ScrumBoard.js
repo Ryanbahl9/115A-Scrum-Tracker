0eb2927473e0bcc428df0dbe364930ee177896a7
Niland Schumacher <lando@littlemouse.local>
2021-10-22 11:01:21 -0700

    moved css into modules

--- src/components/ScrumBoard.js
+++ src/components/ScrumBoard.js
@@ -6,13 +6,13 @@ import UserStoryRow from "./UserStoryRow"
 import { doc, updateDoc } from "firebase/firestore";
 import { Container, Button, Box, TextField, FormControl, MenuItem, Select, InputLabel } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import styles from './ScrumBoard.module.css'
 
 const Board = () => {
     const newColumnRef = useRef(null);
     const [stageTitles, setStageTitles] = useState([]);
     const [stageTitleComponents, setStageTitleComponents] = useState([]);
     let { product } = useContext(UserContext);
-
     const userStoryRef = firestore.collection('userStory');
     let query;
     if(product){
@@ -21,6 +21,8 @@ const Board = () => {
         query = userStoryRef.where('productID', '==', '0');
     }
     let [UserStories, loading] = useCollection(query);
+
+    
 
     const addColumn = async (e) => {
         if (newColumnRef.current.value.length === 0) return;
@@ -31,65 +33,36 @@ const Board = () => {
         });
     }
 
+
     useEffect(() => {
         if(!product || loading) return;
         let tempStageTitles = ["To Do"];
-        let tempStageTitleComponents = [<Box sx={{
-                scrollSnapAlign: "start", 
-                paddingTop: "10px",
-                textAlign: "center",
-                minWidth: "100px",
-                borderBottom: "1px solid black",
-                borderRight: "1px solid black",
-                maxHeight:"30px" }}
-            key={0}>User Stories</Box>, 
-            <Box sx={{ 
-                scrollSnapAlign: "start",
-                paddingTop: "10px",
-                textAlign: "center",
-                minWidth: "200px",
-                borderBottom: "1px solid black",
-                borderRight: "1px solid black",
-                maxHeight: "30px" }}
-            key={1}>To Do</Box>];
-
+        let tempStageTitleComponents = [<Box className={styles.firstStageTitle} key={0}>User Stories</Box>,
+                                        <Box className={styles.stageTitle} key={1}>To Do       </Box>];
         if(product.stages){
-            tempStageTitleComponents = tempStageTitleComponents.concat(product.stages
-                .map((stageTitle, i) => <Box sx={{ paddingTop: "10px",
-                        scrollSnapAlign: "start",
-                        textAlign: "center",
-                        minWidth: "200px",
-                        borderBottom: "1px solid black",
-                        borderRight: "1px solid black",
-                        maxHeight: "30px"  }}
-                    key={i+2}>{stageTitle}<MenuIcon sx={{ marginLeft: "10px", maxHeight: '15px', maxWidth: '15px', cursor: "pointer"}}/></Box>));
+            tempStageTitleComponents = tempStageTitleComponents
+                .concat(product.stages
+                .map((stageTitle, i) => <Box className={styles.stageTitle} key={i+2}>
+                                            {stageTitle}
+                                            <MenuIcon className={styles.titleIcon}/>
+                                        </Box>));
             tempStageTitles = tempStageTitles.concat(product.stages);
         }
 
-        tempStageTitleComponents.push(<Box sx={{scrollSnapAlign: "start",
-                                                paddingTop: "10px",
-                                                textAlign: "center",
-                                                minWidth: "200px",
-                                                borderBottom: "1px solid black",
-                                                maxHeight: "30px" }}
-                                        key={tempStageTitles.length+1}>Completed</Box>);
-
+        tempStageTitleComponents.push(<Box className={styles.lastStageTitle} key={tempStageTitles.length+1}>Completed</Box>);
         tempStageTitles.push("Completed")
         setStageTitleComponents(tempStageTitleComponents);
         setStageTitles(tempStageTitles);
     }, [product, loading]);
 
+
+
     return (<>{
         product ?
-            (<Container sx={{ overflowX: "scroll",
-                              overflowY: "hidden",
-                              scrollSnapType:"x mandatory",
-                              scrollPadding:"5px",
-                              background: "aliceBlue",
-                              height: "90vh" }}>
-                <FormControl size="sm" sx={{ position: "fixed",
-                                             maxWidth:"300px",
-                                             marginTop:"2%"}}>
+            (<Container className={styles.boardContainer}>
+                {/* Probably should make this its own component
+
+                <FormControl size="sm" className={styles.sprintSelector}>
                     <InputLabel id="demo-simple-select-label">
                         Sprint
                     </InputLabel>
@@ -105,29 +78,22 @@ const Board = () => {
                         <MenuItem value={0}>sprint 2</MenuItem>
                         <MenuItem value={0}>sprint 3</MenuItem>
                     </Select>
-                </FormControl>
+                </FormControl> */}
 
-                <Box sx={{display: 'inline-flex',
-                          marginTop: "6%",
-                          maxHeight:"40px",}}>
+                <Box className={styles.stageTitlesContainer}>
                     {stageTitleComponents}
-                    <Box sx={{paddingLeft: "50px",
-                              minWidth: "200px",
-                              paddingRight:"100px"}}>
+                    <Box className={styles.addStage}>
                         <TextField inputRef={newColumnRef} id="outlined-basic" label="Add Stage" variant="outlined" />
                         <Button variant="outlined" onClick={addColumn}>
                             + add stage
                         </Button>
                     </Box>
                 </Box>
-                <Box sx={{ width: `${(stageTitles.length * 200) + 100}px`,
-                           height: "85%",
-                           overflowY: "scroll",
-                           overflowX: "hidden",
-                           scrollSnapType: "y mandatory"}}>
+
+                <Box sx={{width: `${(stageTitles.length * 200) + 100}px`}} className={styles.userStoriesContainer}>
                     {loading && <div>user stories are loading</div> }
                     {!loading && UserStories.docs.map(doc => { 
-                                        return (<UserStoryRow key={doc.id} id={doc.id} data={doc.data()} stageTitles={stageTitles}/>)
+                        return (<UserStoryRow key={doc.id} id={doc.id} data={doc.data()} stageTitles={stageTitles}/>)
                     })}
                 </Box>
             </Container >)
--- src/components/UserStoryRow.js
+++ src/components/UserStoryRow.js
@@ -3,6 +3,7 @@ import { Box, List, ListItem } from '@mui/material';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import { firestore } from './fire';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import styles from './UserStoryRow.module.css';
 
 const UserStoryRow = (props) => {
     console.log("test")
@@ -11,31 +12,22 @@ const UserStoryRow = (props) => {
     const query = taskRef.where('userStoryId', '==', props.id);
     let [tasks, loading] = useCollectionData(query);
 
-    return (<Box sx={{ display: "flex", justifyContent: "start", width: `${(props.stageTitles.length * 200) - 100}px`,}}>
-            <Box sx={{
-                minWidth: "100px", display: "flex", flexDirection: "row", justifyContent:"center", alignItems:"center", height: "200px",
-                borderBottom: "1px solid black"
-            }}>
-                <DragHandleIcon sx={{cursor:"grab"}}/>
+    return (<Box className={styles.outerDiv} sx={{width: `${(props.stageTitles.length * 200) - 100}px`}}>
+                <Box className={styles.handleContainer}>
+                    <DragHandleIcon sx={{cursor:"grab"}}/>
+                </Box>
+                <Box className={styles.stagesDiv} sx={{width:`${props.stageTitles.length * 200}px`}}>
+                    {props.stageTitles.map((title, i)=>
+                        (<Box key={i} className={styles.stage}>
+                            <List className={styles.taskList}>
+                                {loading && <div>loading</div>}
+                                {!loading && tasks && tasks.map(task => {
+                                    if(task.stage === title) return <ListItem>{task.name}</ListItem>
+                                })}
+                            </List>
+                         </Box>))}
+                </Box>
             </Box>
-            <Box sx={{ scrollSnapAlignY: "start",
-                       scrollSnapAlignX: "end",
-                       borderBottom: "1px solid black",
-                       borderLeft: "1px solid black",
-                       width: `${props.stageTitles.length * 200}px`,
-                       height: "200px",
-                       display:"inline-flex"}}>
-
-                {props.stageTitles.map((title, i)=>
-                    (<Box key={i} sx={{borderRight: "1px solid black", minWidth:"200px", minHeight:"200px"}}>
-                        <List sx={{ margin:"0", padding:"0", height:"200px", width:"200px", overflowY:"scroll"}}>
-                        {tasks && tasks.map(task => {
-                            if(task.stage === title) return <ListItem>{task.name}</ListItem>})}
-                        </List>
-                     </Box>))}
-    
-            </Box>
-        </Box>
     )
 }
 
