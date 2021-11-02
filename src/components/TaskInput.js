f9130ca88c87ffb6d8be58f5505e2c35aecaf726
Brandon Kwe <bkwe@DESKTOP-JEO1C3A.localdomain>
2021-11-02 10:48:45 -0700

    added button and input field to create task

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -1,12 +1,37 @@
 import * as React from 'react';
 
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+import { useDocument, useCollection, useDocumentData } from 'react-firebase-hooks/firestore';
+import {firestore} from './fire';
+import UserContext from './UserContext';
+import UserStoryCard from './UserStoryCard';
+import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+
 const TaskInput = (props) => {
-    console.log(props.userStoryId)
+    const createTask = () => {
+        console.log(props.userStoryId)
+    }
 
     return (
-        <div>
-            Task input will go here. It will be bottom aligned.
-        </div>
+        <Stack direction="row" spacing={1}>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '100ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                    <TextField id="filled-basic" label="Add Task" variant="filled" width="110ch"/>
+            </Box>
+            <Button onClick={createTask}>
+                Create Task
+            </Button>
+        </Stack>
     );
 }
 
