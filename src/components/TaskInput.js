379e284414126e9c0f5f98b1a3a802987230ac7d
Brandon Kwe <bkwe@DESKTOP-JEO1C3A.localdomain>
2021-11-02 22:05:09 -0700

    add tasks to user stories

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -6,31 +6,50 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 
 import { useDocument, useCollection, useDocumentData } from 'react-firebase-hooks/firestore';
-import {firestore} from './fire';
+import {firestore, auth} from './fire';
 import UserContext from './UserContext';
 import UserStoryCard from './UserStoryCard';
 import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 
 const TaskInput = (props) => {
+    const [value, setValue] = React.useState('');
+    const handleChange = (event) => {
+        setValue(event.target.value);
+      };
+
     const createTask = () => {
-        console.log(props.userStoryId)
+        if (value == '') return;
+            firestore.collection('userStory').doc(props.userStoryId).update({
+                tasks: arrayUnion({
+                    userId: auth.currentUser.uid,
+                    userStoryId: props.userStoryId,
+                    description: value
+                })
+            })
     }
 
     return (
-        <Stack direction="row" spacing={1}>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '100ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                    <TextField id="filled-basic" label="Add Task" variant="filled" width="110ch"/>
-            </Box>
-            <Button onClick={createTask}>
-                Create Task
-            </Button>
+        <Stack>
+            <Stack direction="row" spacing={1}>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: 1, width: '100ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    >
+                        <TextField id="filled-basic" label="Add Task" variant="filled" width="110ch" onChange = {handleChange}/>
+                </Box>
+                <Button onClick={createTask}>
+                    Create Task
+                </Button>
+            </Stack>
+            {props.taskArray.map(task => (
+                <h3>
+                    -{task.description}
+                </h3>
+            ))}
         </Stack>
     );
 }
