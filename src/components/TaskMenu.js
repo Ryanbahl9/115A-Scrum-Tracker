08a81e6d4e18d7c625c5205ec744514b02c607b8
Niland Schumacher <lando@littlemouse.local>
2021-11-08 16:12:00 -0800

    shows name

--- src/components/TaskMenu.js
+++ src/components/TaskMenu.js
@@ -66,7 +66,7 @@ export default function BasicMenu(props) {
 
     const handleAssignOwner = async (e) => {
       e.preventDefault();
-      await updateDoc(doc(firestore, "task", props.id), {userId: auth.currentUser.uid});
+      await updateDoc(doc(firestore, "task", props.id), {userId: auth.currentUser.uid, displayName: auth.currentUser.displayName});
       handleClose();
     }
 
@@ -95,7 +95,13 @@ export default function BasicMenu(props) {
             >
                 <MenuItem disabled={props.data.stage === 'Complete'} onClick={handleMoveNext}>Move task to next stage</MenuItem>
                 <MenuItem disabled={props.data.stage === 'Queue'} onClick={handleMovePrev}>Move task to prev stage</MenuItem>
-                <MenuItem disabled={props.data.userId} onClick={handleAssignOwner}>Assign yourself to task</MenuItem>
+                <MenuItem disabled={props.data.userId} onClick={handleAssignOwner}>
+                  {props.data.userId ? 
+                    `Assigned to ${props.data.displayName}`
+                  :
+                    <div>Assigne yourself to task</div>
+                  }
+                </MenuItem>
                 <Divider />
                 <MenuItem sx={{color: 'red'}}onClick={handleDelete}>Delete task</MenuItem>
             </Menu>
