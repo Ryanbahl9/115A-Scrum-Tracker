1076856e6fa021a88ab15970008449fc8ca654b8
Matthew Zurlin <mzurlin@ucsc.edu>
2021-11-27 18:54:32 -0800

    Update firebase Data

--- src/components/fire.js
+++ src/components/fire.js
@@ -7,13 +7,13 @@ import 'firebase/compat/analytics';
 firebase.initializeApp({
 
   // Using superChatDemo // this one works and the other doesnt for unknown reason
-  apiKey: "AIzaSyDclao4L0sn-6hSUU5FYAdFSCzDy5TNM2k",
-  authDomain: "a-scrumtracker.firebaseapp.com",
-  projectId: "a-scrumtracker",
-  storageBucket: "a-scrumtracker.appspot.com",
-  messagingSenderId: "1050741778439",
-  appId: "1:1050741778439:web:5f5d1c71faee2118b522a2",
-  measurementId: "G-QL1D4RGPN6"
+  apiKey: "AIzaSyA6NqhANpV81OdPjkAz9-hDjNffmpNegQE",
+  authDomain: "a-167d2.firebaseapp.com",
+  projectId: "a-167d2",
+  storageBucket: "a-167d2.appspot.com",
+  messagingSenderId: "121019454084",
+  appId: "1:121019454084:web:8a589bcaa810dff09628d1",
+  measurementId: "G-5GVS1DGZ3W"
 })
 
 export const auth = firebase.auth();
